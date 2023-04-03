5093f6b1730cc807ef0cfa99792b42212c93e549
saifibrahim <55207510+saifibrahim@users.noreply.github.com>
2023-04-03 12:22:26 +0300

    update

--- core/settings.py
+++ core/settings.py
@@ -133,14 +133,14 @@ LOGIN_REDIRECT_URL = 'home'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
