8fbd9774d56fcf7c866871b560d56b10560bbde1
saifibrahim <55207510+saifibrahim@users.noreply.github.com>
2023-06-14 15:18:59 +0300

    update serviceworker

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -1,26 +1,47 @@
-var staticCacheName = 'creative-v2';
+var staticCacheName = 'qi-creative-v2' + new Date().getTime();
+var filesToCache = [
+    '/offline',
+    '/static/css/aos.css',
+    '/static/css/bootstrap.min.css.css',
+    '/static/css/index-ce0453.css',
+    '/images/Burger_button.svg',
+    '/images/Search.svg',
+    '/images/profile.svg',
+    '/images/qi-logo.svg',
+    '/images/profile.svg',
+    '/images/qi-logo-yellow.svg'
+];
 
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(staticCacheName).then(function(cache) {
-            return cache.addAll([
-                '',
-            ]);
+            return cache.addAll(filesToCache);
         })
     );
 });
 
-self.addEventListener('fetch', function(event) {
-    var requestUrl = new URL(event.request.url);
-    if (requestUrl.origin === location.origin) {
-        if ((requestUrl.pathname === '/')) {
-            event.respondWith(caches.match(''));
-            return;
-        }
-    }
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                .filter(cacheName => (cacheName.startsWith("qi-creative-pwa")))
+                .filter(cacheName => (cacheName !== staticCacheName))
+                .map(cacheName => caches.delete(cacheName))
+            );
         })
     );
+});
+
+// Serve from Cache
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => {
+            return response || fetch(event.request);
+        })
+        .catch(() => {
+            return caches.match('offline');
+        })
+    )
 });
