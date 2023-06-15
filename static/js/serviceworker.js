b2ba1b561baac80428d1f54f611282f6f48120b7
saifibrahim <55207510+saifibrahim@users.noreply.github.com>
2023-06-15 11:26:09 +0300

    fix service worker

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -1,10 +1,6 @@
 var staticCacheName = 'qi-creative-v2' + new Date().getTime();
 var filesToCache = [
-    '/',
-    '/business_order/new/',
-    '/service_order/new/',
-    '/business_order/requests/',
-    '/service_order/requests/'
+    '/'
 ];
 
 self.addEventListener("install", event => {
