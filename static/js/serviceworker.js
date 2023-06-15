f2cb455fbd42fc2a0e1ec85b1926e364828c70f2
saifibrahim <55207510+saifibrahim@users.noreply.github.com>
2023-06-15 09:07:32 +0300

    update serviceworker

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -43,7 +43,7 @@ self.addEventListener("fetch", event => {
             return response || fetch(event.request);
         })
         .catch(() => {
-            return caches.match('offline');
+            return caches.match('');
         })
     )
 });
