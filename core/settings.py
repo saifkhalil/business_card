58e05128cccaa1205417e947d19e17dc41e952ce
saifibrahim <55207510+saifibrahim@users.noreply.github.com>
2023-06-15 12:27:19 +0300

    fix manifest

--- core/settings.py
+++ core/settings.py
@@ -332,28 +332,19 @@ PWA_APP_START_URL = '/'
 PWA_APP_STATUS_BAR_COLOR = '#f0cc00'
 PWA_APP_ICONS = [
     {
-        "src": "/static/images/android-chrome-192x192.png",
-        "sizes": "192x192",
+        "src": "/static/images/qi_160.png",
+        "sizes": "160x160",
         "type": "image/png",
-        "purpose": "any"
     },
     {
         "src": "/static/images/android-chrome-192x192.png",
         "sizes": "192x192",
         "type": "image/png",
-        "purpose": "maskable"
     },
     {
         "src": "/static/images/android-chrome-512x512.png",
         "sizes": "512x512",
         "type": "image/png",
-        "purpose": "any"
-    },
-    {
-        "src": "/static/images/android-chrome-512x512.png",
-        "sizes": "512x512",
-        "type": "image/png",
-        "purpose": "maskable"
     }
 ]
 PWA_APP_ICONS_APPLE = [
